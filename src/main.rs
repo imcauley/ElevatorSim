f2ea0cb2f3af43f6e3b214123bf8e28ea3655fa2
Isaac McAuley <mcauleyisaac@gmail.com>
2022-07-31 13:00:59 -0400

    pivoted to elevator simulator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 
+use std::char::from_digit;
+
 use bracket_lib::prelude::*;
 
 const FRAME_DURATION: f32 = 75.0;
@@ -15,74 +17,50 @@ enum GameMode {
     Playing,
 }
 
-struct Player {
-    x: i32,
-    y: i32,
-    direction_x: f32,
-    direction_y: f32,
-    velocity: f32,
+struct Person {
+    origin: i32,
+    destination: i32,
+    wait_time: i32,
+}
+
+struct Elevator {
+    floor: i32,
+    capacity: i32,
 }
 
 struct State {
     mode: GameMode,
     frame_time: f32,
-    player: Player,
+    elevator: Elevator,
 }
 
-impl Player {
-    fn new(x: i32, y: i32) -> Self {
-        Player {
-            x,
-            y,
-            direction_x: 0.0,
-            direction_y: 0.0,
-            velocity: 0.0,
-        }
-    }
-
-    fn accelerate(&mut self, amount: f32) {
-        if amount > MAX_ACCELERATION {
-            self.velocity += MAX_ACCELERATION;
-        } else {
-            self.velocity += amount;
-        }
-
-        if self.velocity > MAX_VELOCITY {
-            self.velocity = MAX_VELOCITY;
-        }
-    }
-
-    fn point(&mut self, x: f32, y: f32) {
-        self.direction_x += x;
-        self.direction_y += y;
-
-        let magnitude = (self.direction_x.powf(2.0) + self.direction_x.powf(2.0))
-            .sqrt()
-            .abs();
-
-        if magnitude == 0.0 {
-            return;
-        }
-
-        self.direction_x /= magnitude;
-        self.direction_y /= magnitude;
-    }
-
-    fn tick(&mut self) {
-        self.x += (self.velocity * self.direction_x).round() as i32;
-        self.y += (self.velocity * self.direction_y).round() as i32;
-
-        if self.velocity > 0.0 {
-            self.velocity -= RESISTENCE;
-        } else {
-            self.velocity = 0.0;
+impl Elevator {
+    fn new() -> Self {
+        Elevator {
+            floor: 1,
+            capacity: 0,
         }
     }
 
     fn render(&mut self, ctx: &mut BTerm) {
-        ctx.set(self.x, self.y, RED, BLACK, to_cp437('#'));
-        ctx.set(self.x, self.y + 1, YELLOW, BLACK, to_cp437('#'));
-        ctx.set(self.x, self.y + 2, YELLOW, BLACK, to_cp437('#'));
+        ctx.set(9, 9, RED, BLACK, to_cp437('#'));
+        ctx.set(9, 10, RED, BLACK, to_cp437('#'));
+        ctx.set(9, 11, RED, BLACK, to_cp437('#'));
+        ctx.set(9, 12, RED, BLACK, to_cp437('#'));
+
+        ctx.set(11, 9, RED, BLACK, to_cp437('#'));
+        ctx.set(11, 10, RED, BLACK, to_cp437('#'));
+        ctx.set(11, 11, RED, BLACK, to_cp437('#'));
+        ctx.set(11, 12, RED, BLACK, to_cp437('#'));
+
+        ctx.set(10, 9, RED, BLACK, to_cp437('#'));
+        ctx.set(10, 12, RED, BLACK, to_cp437('#'));
+
+        let floor_string = (self.floor as u32).to_string();
+
+        for (i, c) in floor_string.chars().enumerate() {
+            ctx.set(9 + i, 8, RED, BLACK, to_cp437(c));
+        }
     }
 }
 
@@ -90,7 +68,7 @@ impl State {
     fn new() -> Self {
         State {
             mode: GameMode::Menu,
-            player: Player::new(5, 25),
+            elevator: Elevator::new(),
             frame_time: 0.0,
         }
     }
@@ -112,45 +90,13 @@ impl State {
 
     fn start_game(&mut self) {
         self.mode = GameMode::Playing;
-        self.player = Player::new(5, 25);
+        self.elevator = Elevator::new();
         self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        let mut x_dir: f32 = 0.0;
-        let mut y_dir: f32 = 0.0;
-
-        ctx.cls_bg(NAVY);
-        self.frame_time += ctx.frame_time_ms;
-
-        if let Some(VirtualKeyCode::W) = ctx.key {
-            y_dir = -1.0;
-        }
-        if let Some(VirtualKeyCode::A) = ctx.key {
-            x_dir = -1.0;
-        }
-        if let Some(VirtualKeyCode::S) = ctx.key {
-            y_dir = 1.0;
-        }
-        if let Some(VirtualKeyCode::D) = ctx.key {
-            x_dir = 1.0;
-        }
-
-        if self.frame_time > FRAME_DURATION {
-            self.frame_time = 0.0;
-            if x_dir != 0.0 || y_dir != 0.0 {
-                self.player.accelerate(1.0);
-            }
-            self.player.point(x_dir, y_dir);
-            self.player.tick();
-        }
-
-        self.player.render(ctx);
-
-        if self.player.y > SCREEN_HEIGHT {
-            self.mode = GameMode::Menu;
-        }
+        self.elevator.render(ctx);
     }
 }
 
@@ -169,59 +115,4 @@ fn main() -> BError {
         .build()?;
 
     main_loop(context, State::new())
-}
-
-#[cfg(test)]
-mod tests {
-    use more_asserts::{assert_gt, assert_le};
-
-    use super::MAX_VELOCITY;
-    use crate::Player;
-
-    #[test]
-    fn player_doesnt_move() {
-        let mut player = Player::new(0, 0);
-        player.tick();
-
-        assert_eq!(player.x, 0);
-        assert_eq!(player.y, 0);
-    }
-
-    #[test]
-    fn player_move_right() {
-        let mut player = Player::new(0, 0);
-
-        player.point(1.0, 0.0);
-        player.accelerate(1.0);
-        player.tick();
-
-        assert_gt!(player.x, 0);
-    }
-
-    #[test]
-    fn player_move_up() {
-        let mut player = Player::new(0, 0);
-
-        player.point(0.0, 1.0);
-        player.accelerate(1.0);
-        player.tick();
-
-        assert_gt!(player.y, 0);
-    }
-
-    #[test]
-    fn player_doesnt_exceed_max_velocity() {
-        let mut player = Player::new(0, 0);
-
-        player.point(1.0, 0.0);
-        player.accelerate(1.0);
-        player.accelerate(1.0);
-        player.accelerate(1.0);
-        player.accelerate(1.0);
-        player.accelerate(1.0);
-        player.tick();
-
-        assert_le!(player.velocity, MAX_VELOCITY);
-        assert_gt!(player.velocity, 0.0);
-    }
 }
